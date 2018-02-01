b4eac7868440381c402192b6c6417447e90e6346
Trung H. Tran <tranhoangtrung.nob@gmail.com>
2018-02-01 21:27:00 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// var url = "https://api.nasa.gov/planetary/apod?api_key=u6FlZ9A0Mow32d0ExoYEU5Btyne17lYyovYxeE3l";
+
+$(function() {
+  $( "#datepicker" ).datepicker();
+  // var url = "https://epic.gsfc.nasa.gov/api/enhanced/date/2018-01-02"
+  // $.ajax({
+  //   url: url,
+  //   success: function(result){
+  //     $("#copyright").html(JSON.stringify(result, null, 4));
+  //   }  
+  // });
+});
