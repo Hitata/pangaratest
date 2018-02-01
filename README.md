c456bdca4e9ebe76d10f0d543f5ba27dd144e481
Trung H. Tran <tranhoangtrung.nob@gmail.com>
2018-02-02 01:38:27 +0900

    2nd commit

--- README.md
+++ README.md
@@ -1,21 +1,42 @@
+# Overview status
+* Javascript Logic 70%
+* Design 50%
 
-# demo
-https://api.nasa.gov/index.html#live_example
+# Time spend
+* 30-45 minutes reading/testing out Nasa EPIC API
+* 15 minutes thinking if I should use React. Decided not to, too risky as it might take too much setting up & get it to work on Lytmus
+* 15-30 minutes decide on Jquery, JqueryUI, then changed to bootstrap, bootstrap-datepicker,...  
+* 30 minutes coding
+* 1 hour taking a break to get late late dinner
+* 1:30 continue coding. Realize 1 hour left for question 2.
 
-# API key 
+# Problem while work
+* API document was clear when I first look at how to form a image_url
+* Decide on what to use took sometime, can't or didnt want to use scss, jsx, and other pre-compile things since it might not work on Lytmus.
+* 5-6 hours straight coding is not ideal for a test, with 2 question, no break time (Feel like its against the idea of freelance)
+* Polishing things takes time. I'm not at level of coding a production product in this time frame.
+
+# Todo
+* Pause and Resume functions
+* Self update information panel
+* Polish Design
+* Refactor javascript code
+* error handling.
+* organize files & replace CDNs
+* Testing on Firefox (Chrome is soso)
+
+# Memos
+## API key 
 u6FlZ9A0Mow32d0ExoYEU5Btyne17lYyovYxeE3l
 
-# API document
+## API document
 https://epic.gsfc.nasa.gov/about/api
 
-# jquery ui
-http://jqueryui.com/button/
-
-# API
+## API
 https://api.nasa.gov/EPIC/api/natural?api_key=DEMO_KEY
 
-# see image
+## see image
 https://epic.gsfc.nasa.gov/
 
-# image link
+## image link
 https://epic.gsfc.nasa.gov/archive/natural/2018/01/30/thumbs/epic_1b_20180130000830.jpg
--- script.js
+++ script.js
@@ -1,12 +1,71 @@
-// var url = "https://api.nasa.gov/planetary/apod?api_key=u6FlZ9A0Mow32d0ExoYEU5Btyne17lYyovYxeE3l";
-
 $(function() {
+  //variables
+  var base_api_url = "https://api.nasa.gov/EPIC/api/"
+  var api_key = "u6FlZ9A0Mow32d0ExoYEU5Btyne17lYyovYxeE3l";
+  var carousel_inner = $("#carouselInner");
+  var carousel_indicators = $(".carousel-indicators");
+
   $( "#datepicker" ).datepicker();
-  // var url = "https://epic.gsfc.nasa.gov/api/enhanced/date/2018-01-02"
-  // $.ajax({
-  //   url: url,
-  //   success: function(result){
-  //     $("#copyright").html(JSON.stringify(result, null, 4));
-  //   }  
-  // });
+  var image_base_url = "https://epic.gsfc.nasa.gov/archive/";
+  var result = [];
+
+  function create_api_url (epic_type, date) {
+    return base_api_url + epic_type + "/date/" + date + "?api_key=" + api_key;
+  }
+
+  function build_image_url (epic_type, date, image_name) {
+    var tmp_date = date.replace(/-/g, "/");
+    return image_base_url + epic_type + "/" + tmp_date + "/png/" + image_name + ".png";
+  }
+
+  function build_carousel_images (items) {
+    var carousel_items = '<div class="carousel-item"><img class="d-block w-100" src="'
+    carousel_items += items.join('"></div><div class="carousel-item"><img class="d-block w-100" src="') + '</div>';
+    return carousel_items
+  }
+
+  function build_carousel_tabs (items) {
+    var carousel_tabs = ""
+    for (var i = 0; i < items.length; i++) {
+      carousel_tabs += '<li class="carousel-tab" data-target="#nasaEpicCarousel"></li>'
+    }
+    return carousel_tabs
+  }
+
+  $( "#submit" ).click(function() {
+    var date = $("#datepicker").val();
+    var epic_type = $("input[name='epicType']").val();
+    var api_url = create_api_url(epic_type, date);
+
+    if (date.length === 0 ) {
+      alert("Please input a Date");
+      return;
+    }
+    if (epic_type.length === 0 ) {
+      alert("Please input a EPIC Type");
+      return;
+    }
+
+    $.getJSON(api_url, function(result) {
+      var items = result.map(function (item) {
+        return build_image_url(epic_type, date, item.image)
+      });
+
+      carousel_inner.empty();
+
+      if (items.length) {
+        var list = build_carousel_images(items)
+        carousel_inner.append(list)
+        var tabs = build_carousel_tabs(items)
+        carousel_indicators.append(tabs)
+        $(".carousel-item:first").addClass("active")
+        $(".carousel-tab:first").addClass("active")
+
+        $('.carousel').carousel({
+          interval: 2000,
+          pause: false
+        });
+      }
+    });
+  });
 });
